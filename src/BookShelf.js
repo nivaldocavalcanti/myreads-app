d9a3f5955e35f31cae2bf70242649268ef17903b
Nivaldo Cavalcanti <nivalduh@gmail.com>
2021-01-06 05:42:41 -0300

    refactor: Changing BookShelf component to functional component as suggested by code review

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
-class BookShelf extends Component {
-    static propTypes = {
-        shelfTitle: PropTypes.string.isRequired,
-        books: PropTypes.array.isRequired,
-        onUpdate: PropTypes.func.isRequired
-    };
+const BookShelf = ({ shelfTitle, books, onUpdate }) => (
+    <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfTitle}</h2>
+        <div className="bookshelf-books">
+            <ol className="books-grid">
+            {books.map(book=>(
+                <li key={book.id}>
+                    <Book book={book} onUpdateShelf={onUpdate} />
+                </li>
+            ))}
+            </ol>
+        </div>
+    </div>
+);
 
-    render() {
-        const { shelfTitle, books } = this.props;
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelfTitle}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {books.map(book=>(
-                        <li key={book.id}>
-                            <Book book={book} onUpdateShelf={this.props.onUpdate} />
-                        </li>
-                    ))}
-                    </ol>
-                </div>
-            </div>
-        );
-    }
-}
+BookShelf.propTypes = {
+    shelfTitle: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired,
+    onUpdate: PropTypes.func.isRequired
+};
+
 
 export default BookShelf;
